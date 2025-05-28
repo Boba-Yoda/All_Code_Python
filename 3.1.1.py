27adfeff792d446796c30e644c2cea35ee4fd25a
Boba-Yoda <anand7942@mydusd.org>
2025-05-28 12:04:18 -0700

    Create 3.1.1.py

---
+++ 3.1.1.py
@@ -0,0 +1,14 @@
+#Nihtahli Anand
+#3.1.1 Python Version
+"""
+This code utlizes the print function
+It prints 'I am coding in Python'
+"""
+
+print("I am coding in Python!")
+
+def text_prompt():
+    name = input("What is your name?")
+    print ("Hello" + name)
+
+text_prompt()
